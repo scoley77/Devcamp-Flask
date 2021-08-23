cb705d12fd87817eb6cb2f5e19aeafed9db1b7cc
Sara Coley <79733957+scoley77@users.noreply.github.com>
2021-08-23 16:57:41 -0600

    Add files via upload

--- hello-flask/app.py
+++ hello-flask/app.py
@@ -1,10 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-  return 'Hey Flask'
+    return "Hey Flask"
+
 
 if __name__ == '__main__':
-  app.run(debug=True)
+    app.run(debug=True)
